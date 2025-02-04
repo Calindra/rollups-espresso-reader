1360e8af03a10dac700fdaf85995c64eecb3914e
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2025-02-04 21:20:07 +0000

    ci: patch5

--- ci/scripts/run-espresso.sh
+++ ci/scripts/run-espresso.sh
@@ -3,4 +3,4 @@ set -e
 
 # docker run --network=host -e ESPRESSO_SEQUENCER_L1_PROVIDER=http://localhost:8545/ -e ESPRESSO_SEQUENCER_API_PORT=24000 ghcr.io/espressosystems/espresso-sequencer/espresso-dev-node:main
 
-docker run -d --network=host -e ESPRESSO_SEQUENCER_L1_PROVIDER=http://localhost:8545/ -e ESPRESSO_SEQUENCER_API_PORT=24000 ghcr.io/espressosystems/espresso-sequencer/espresso-dev-node:20241120-patch4
+docker run -d --network=host -e ESPRESSO_SEQUENCER_L1_PROVIDER=http://localhost:8545/ -e ESPRESSO_SEQUENCER_API_PORT=24000 --name espresso-dev-node ghcr.io/espressosystems/espresso-sequencer/espresso-dev-node:20241120-patch5
