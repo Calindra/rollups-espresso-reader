87e521fd88d0c6a5ee6523ed3868bd0a90027824
Garry Dias <magarrett.dias@calindra.com.br>
2025-02-05 17:32:28 -0300

    chore: rodando apontando pro anvil e espresso local em vez do dcaf e sepolia

--- ci/scripts/run-espresso.sh
+++ ci/scripts/run-espresso.sh
@@ -3,4 +3,9 @@ set -e
 
 # docker run --network=host -e ESPRESSO_SEQUENCER_L1_PROVIDER=http://localhost:8545/ -e ESPRESSO_SEQUENCER_API_PORT=24000 ghcr.io/espressosystems/espresso-sequencer/espresso-dev-node:main
 
-docker run -d --network=host -e ESPRESSO_SEQUENCER_L1_PROVIDER=http://localhost:8545/ -e ESPRESSO_SEQUENCER_API_PORT=24000 --name espresso-dev-node ghcr.io/espressosystems/espresso-sequencer/espresso-dev-node:20241120-patch5
+# docker run --network=host -e ESPRESSO_SEQUENCER_L1_PROVIDER=http://localhost:8545/ -e ESPRESSO_SEQUENCER_API_PORT=24000 ghcr.io/espressosystems/espresso-sequencer/espresso-dev-node:main
+# docker network create my_bridge
+docker rm espresso-dev-node || true
+docker run --network my_bridge --rm --name devnet -p 8545:8545 -d cartesi/rollups-node-devnet:devel
+sleep 10
+docker run --network my_bridge -e ESPRESSO_SEQUENCER_L1_PROVIDER=http://devnet:8545/ -e ESPRESSO_SEQUENCER_API_PORT=24000 -p 24000:24000 --name espresso-dev-node ghcr.io/espressosystems/espresso-sequencer/espresso-dev-node:20241120-patch5
